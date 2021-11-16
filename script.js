8f26c78e82fdc10dfe6c40f1d63b76a3e7064de6
kemikal <janne@kemikal.org>
2021-11-16 11:07:02 +0100

    adds js magic to project

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 console.log("Hej från JS!");
+
+// var x = "Hej";
+
+let x = "Hej";
+
+const y = 10;
+
+let klass = "FED21D";
+
+let helloString = x + " " + klass + "!";
+
+console.log(helloString);
+
+// x += " på dig!"
+
+let summa = x + y;
+
+console.log("summa", summa);
+
+console.log(x);
+
+let z = true;
+
+let root = document.getElementById("root");
+// textBox.insertAdjacentHTML("afterbegin", helloString);
+// textBox.innerHTML = helloString;
+
+let fName = prompt("Hej vad heter du?");
+let lName = prompt("Och i efternamn då?");
+
+let userName = fName + " " + lName;
+
+//alert("Hej du heter visst: " + userName)
+
+console.log("userName", userName);
+
+//root.insertAdjacentHTML("afterbegin", "<div class='userBox' id='user'>" + userName + "</div>");
+
+document.getElementById("root").insertAdjacentHTML("afterbegin", "<div class='userBox' id='user'>" + userName + "</div>");
