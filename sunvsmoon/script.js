43fa867a7dab08e57b43b0c6e2b84a70a2a9d51f
kemikal <janne@kemikal.org>
2021-11-24 09:42:42 +0100

    adds folder structure to sunvsmoon

--- sunvsmoon/script.js
+++ sunvsmoon/script.js
@@ -1,7 +1,7 @@
 // Visa en bild på en sol
 let sunvsmoon = document.createElement("img");
 sunvsmoon.id = "sunvsmoon";
-sunvsmoon.src = "sun.jpg";
+sunvsmoon.src = "img/sun.jpg";
 
 document.body.append(sunvsmoon);
 
@@ -12,17 +12,13 @@ sunvsmoon.addEventListener("click", () => {
     console.log("state på src", sunvsmoon.getAttribute("src"));
 
     // Är det en sol de har klickat på, byt bild till en måne
-    if(sunvsmoon.getAttribute("src") === "sun.jpg") {
-        sunvsmoon.setAttribute("src", "moon.jpg")
-
+    if(sunvsmoon.getAttribute("src") === "img/sun.jpg") {
+        sunvsmoon.setAttribute("src", "img/moon.jpg")
     } else {
         
          // Är det en måne de har klickat på byt bild till en sol
-         sunvsmoon.setAttribute("src", "sun.jpg")
-
+         sunvsmoon.setAttribute("src", "img/sun.jpg")
     }
-
-   
 })
 
 
